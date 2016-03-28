6efc81c632b3ca26e371a92368239d6953248d5e
Alexander Agranovich <aag@alfapeople.com>
2016-03-28 22:37:49 +0300

    Initial commit from a template

---
+++ DaxBootStrap/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DaxBootStrap.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DaxBootStrap.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ DaxBootStrap/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaxBootStrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="DAX Service";
+return View();
+        }
+    }
+}
